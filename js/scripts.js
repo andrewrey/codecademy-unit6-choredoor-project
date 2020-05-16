65a0d1576d78855be83f37266f2c490c5016643d
Andrew Reynolds <andrew_reynolds@mac.com>
2020-05-16 14:41:10 -0700

    up to partII ACT like a game

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,44 @@ let door3 = document.querySelector('#door3');
 let botDoor = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 let beachDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 let spaceDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
 
 //Event Listeners 
 door1.addEventListener('click', (e)=>{
-  e.target.src = botDoor;
+  e.target.src = openDoor1;
 })
 
 door2.addEventListener('click', (e)=>{
-  e.target.src = beachDoor;
+  e.target.src = openDoor2;
 })
 
 door3.addEventListener('click', (e)=>{
-  e.target.src = spaceDoor;
+  e.target.src = openDoor3;
 })
+
+
+/// Function ///
+
+//Random door generator
+
+function randomChoreDoorGenerator(){
+  let choreDoor = Math.floor(Math.random() * numClosedDoors);
+  if(choreDoor === 0){
+    openDoor1 = botDoor;
+    openDoor2 = beachDoor;
+    openDoor3 = spaceDoor;
+  } else if (choreDoor === 1){
+    openDoor2 = botDoor;
+    openDoor1 = beachDoor;
+    openDoor3 = spaceDoor;
+  } else if (choreDoor === 2){
+    openDoor3 = botDoor;
+    openDoor1 = spaceDoor;
+    openDoor2 = beachDoor;
+  }
+}
+
+randomChoreDoorGenerator();
